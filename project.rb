27f7e74e2e62c5d65414a95b0b5e858828dba460
Andrew Carpenter <acarpen@wested.org>
2010-08-24 13:57:58 -0700

    Fix commiting when not current directory

--- project.rb
+++ project.rb
@@ -1,4 +1,6 @@
 class Project
+  Grit.debug = true
+  
   class CommitError < StandardError; end
   def self.all
     data = YAML.load( File.open(File.join(File.dirname(__FILE__), 'projects.yml') ) )
@@ -19,8 +21,13 @@ class Project
   end
   
   def add_and_commit_all!(message)
-    repo.add(repo.status.untracked.map{|file, status| file }) or raise CommitError, "cannot add all files"
-    repo.commit_all(message) or raise CommitError, "cannot commit all files"
+    Dir.chdir(path) do
+      repo.status.untracked.map do |file, status|
+        repo.add(file)
+      end
+      
+      repo.commit_all(message) or raise CommitError, "cannot commit all files"
+    end
   end
   
   def status
